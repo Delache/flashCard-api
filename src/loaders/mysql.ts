d18b9c4410a2755ea12a3031c2aa547203b7f10f
Delache <delache33@gmail.com>
2019-12-10 14:33:58 +0100

    fix: ip

--- src/loaders/mysql.ts
+++ src/loaders/mysql.ts
@@ -3,17 +3,25 @@ import { DbHandler } from '../repository/db.handler';
 
 export default async () => {
 
+  console.log(process.env.WILD_API_DB_PORT);
   console.log(process.env.WILD_API_DB_HOST);
+  console.log(process.env.WILD_API_DB_USER);
+  console.log(process.env.WILD_API_FUMAINERIE_DB_PASSWORD);
 
   const connexion = mysql.createConnection({
     host: process.env.WILD_API_DB_HOST,
-    port: parseInt(process.env.WILD_API_DB_PORT || '3306', 10),
+    port: 1234,
     user: process.env.WILD_API_DB_USER,
     password: process.env.WILD_API_FUMAINERIE_DB_PASSWORD,
     database: process.env.WILD_API_DB_DATABASE,
   });
 
   DbHandler.getInstance(connexion);
+
+  connexion.connect(function(err) {
+    if (err) { throw err; }
+    console.log('Connected!');
+  });
 
   return connexion;
 };
