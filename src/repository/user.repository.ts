3cc1a4f9a6a88f089f3aafb1c7591741e2b4bd84
DOligex <olinours@yahoo.fr>
2019-12-11 10:02:43 +0100

    feat: user database request done and work

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -1,9 +1,13 @@
 import { DbHandler } from './db.handler';
+import { User } from 'src/models/user';
 
 export class UserRepository {
 
     private GET_ALL = 'SELECT * FROM user;';
-    private GET_BY_ID = 'SELECT * FROM user where id =';
+    private GET_BY_ID = 'SELECT * FROM user where id = ?';
+    private POST_BY_ID = 'INSERT INTO user SET ?';
+    private PUT_BY_ID = 'UPDATE user SET ? WHERE id = ?';
+    private DEL_BY_ID = 'DELETE FROM user WHERE id = ?';
 
     private db: DbHandler;
 
@@ -13,23 +17,32 @@ export class UserRepository {
     }
 
     async findAll() {
-        // this.db.query(this.GET_ALL).then((result) => {
-        //     console.log(result);
+        // this.db.query(this.GET_ALL).then((reust) => {
+        //     console.log(reust);
         // });
 
         // Ou alors on peut ecrire =>
-
         const result = await this.db.query(this.GET_ALL);
         return result;
     }
 
     async findById(id: number) {
-        const user = await this.db.query(this.GET_BY_ID + id);
+        const user = await this.db.query(this.GET_BY_ID , id);
         return user;
     }
 
-    save(user: any) {
-        // votre code ici
+    async save(user: User) {
+        const postUser = await this.db.query(this.POST_BY_ID, user);
+        return postUser;
+    }
 
+    async modify(user: User, id: number) {
+        const modifyUser = await this.db.query(this.PUT_BY_ID, [user, id]);
+        return modifyUser;
+    }
+
+    async delete(id: number) {
+        const deleteUser = await this.db.query(this.DEL_BY_ID , id);
+        return deleteUser;
     }
 }
